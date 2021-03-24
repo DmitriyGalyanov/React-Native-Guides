e9d886546f0575dd7da66ebcf8d9b617fc071651
Dmitriy Galyanov <dimagalyanov@gmail.com>
2021-03-24 20:22:45 +0300

    add redux-example (it also works)

---
+++ redux/store.ts
@@ -0,0 +1,56 @@
+// @ts-ignore
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import {
+	combineReducers,
+	configureStore,
+	getDefaultMiddleware,
+	// @ts-ignore
+} from '@reduxjs/toolkit';
+// @ts-ignore
+import { useDispatch } from 'react-redux';
+
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+	// @ts-ignore
+} from 'redux-persist';
+
+import {
+	webViewSliceReducer,
+} from './stateSlices';
+
+const persistConfig = {
+	key: 'root',
+	storage: AsyncStorage,
+};
+
+const rootReducer = combineReducers({
+	webViewSlice: webViewSliceReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+	reducer: persistedReducer,
+	middleware: getDefaultMiddleware({
+		serializableCheck: {
+			/*eslint-disable comma-dangle*/
+			ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+			/*eslint-enable comma-dangle*/
+		},
+	}),
+});
+
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+export const persistor = persistStore(store);
