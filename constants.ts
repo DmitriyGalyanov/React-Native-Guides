3d953681c5c68db7125d5008f166932f2a0a0582
Dmitriy Galyanov <dimagalyanov@gmail.com>
2021-05-12 19:03:30 +0300

    adjust constants (splitting by sections, ...
    new sections essential for the new mechanism)

--- constants.ts
+++ constants.ts
@@ -1,6 +1,8 @@
 //@ts-nocheck
+/* dimensions */
 //window dimensions
 import {Dimensions} from 'react-native';
+import { Replaces } from './commonTypes';
 
 const {
 	width: windowWidth,
@@ -8,35 +10,69 @@ const {
 }: {width: number; height: number} = Dimensions.get('window');
 export {windowWidth, windowHeight};
 
-//styles
+//game dimensions
+
+
+
+/* styles */
 //colors
 export enum colors {
-	'white' = '#fff',
-	'black' = '#000',
-	'green' = '#25CC68',
-	'gray' = '#9E9D9D',
-	'red' = '#F92762',
+	white = '#fff',
+	black = '#000',
+	green = '#25CC68',
+	gray = '#9E9D9D',
+	red = '#F92762',
+	modalBgColor = 'rgba(45, 1, 22, 0.8)',
 };
 
-// not game-logics related
-export const appsflyerDevKey: string = 'key_here';
 
-//loading alert(s)
-//duplicated values are commented
+/* not game-logics related */
+export const appsflyerDevKey: string = 'hLTkg5KEsQa3VfyHMbofJ3';
+export const requiredXValue: number = 11;
+
+// values used for AppsFlyer OneLink and Facebook Deep Link data parsing
+// via helpers.cutAndReplace()
+// essential to Deep Link Event Handling
+export const JAVA_DEEP_LINK_TO_JS_EVENT_KEY = "JavaDeepLinkToJs";
+export const DEEP_LINK_TARGET_URI_KEY = "deepLinkTargetUri";
+export const DEEP_LINK_IS_NOT_AVAILABLE_KEY = "is not available";
+
+// essential to helpers.cutAndReplace()
+export const oneLinkAdNameParsingSeparator: string = '|S|';
+export const oneLinkAdSetNameParsingSeparator: string =
+	oneLinkAdNameParsingSeparator;
+export const oneLinkAdCampaignNameParsingSeparator: string =
+	oneLinkAdNameParsingSeparator;
+export const oneLinkAdNameParsingReplaces: Replaces = {
+	searchValue: '|U|', replaceValue: '&',
+};
+export const oneLinkAdSetNameParsingReplaces: Replaces =
+	oneLinkAdNameParsingReplaces;
+export const oneLinkAdCampaignNameParsingReplaces: Replaces =
+	oneLinkAdNameParsingReplaces;
+
+export const fbDeepLinkParsingSeparator: string = '|S|';
+export const fbDeepLinkParsingReplaces: Replaces = {
+	searchValue: '|U|', replaceValue: '&',
+};
+
+//loading alert
+//essential values
+export const loadingAlertText: string = 'loading...';
+export const loadingAlertColor1: string = 'rgb(57,103,196)';
+export const loadingAlertColor2: string = 'rgb(55,143,191)';
+//some of those (possibly -- all) should be removed when a Component is chosen
+//Component dependant values
+//CenteredAlert
+// number between 20 and 36
+export const loadingAlertIndicatorSize: 'small' | 'large' | number = 24;
 //CenteredLoadingBar
+// range: 0 -- 100
 export const loadingAlertInitProgress: number = 5;
+// range: loadingAlertInitProgress -- 100
 export const loadingAlertMaxProgress: number = 92;
 export const loadingAlertAnimDuration: number = 2700;
-export const loadingAlertText: string = 'loading...';
-export const loadingAlertColor1: string = 'rgb(110,160,67)';
-export const loadingAlertColor2: string = 'rgb(159,239,97)';
 
-//CenteredAlert
-// export const loadingAlertText: string = 'loading...';
-// export const loadingAlertColor1: string = 'rgb(166,196,57)';
-// export const loadingAlertColor2: string = 'rgb(155,191,55)';
-//number between 20 and 36
-export const loadingAlertIndicatorSize: 'small' | 'large' | number = 'small';
 
 
 // single match creation for apps with coefficients dropdowns -- start
