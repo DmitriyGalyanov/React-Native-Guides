a48d798e8e2e4bbb652bd3eea315b601f894b73d
Dmitriy Galyanov <dimagalyanov@gmail.com>
2021-05-12 19:04:33 +0300

    add refactored webViewSlice (deprecated is not removed)

--- redux/stateSlices/webViewSlice.ts
+++ redux/stateSlices/webViewSlice.ts
@@ -1,49 +1,96 @@
 //@ts-nocheck
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
-// import { RootState } from '../store';
 
 
 export interface WebViewSliceState {
-	remoteConfigUrl: string,
-	deepLinkGatheredData: string,
-	finalUrl: string,
-	shouldRenderWebViewExclusively: boolean,
+	isOnline: boolean, //ok
+	//ok | is it? (can be local variable ? ) todo
+	firebaseUrlBasis: string,
+	// OneLink is AppsFlyers Deep Link technology
+	parsedOneLinkAdCampaignName: string, //ok
+	deepLinkParsedData: string, //ok
+	theXValue: string, //can be packageName for example
+	// | is it required? (can be local variable ? ) todo
+	openAtInstallUrl: string, //ok
+	didOpenInstallUrl: boolean,
+	lastOpenedUrl: string,
+	renderWebView: boolean, //ok
+	renderGame: boolean, //ok
 };
 const initialState: WebViewSliceState = {
-	remoteConfigUrl: '',
-	deepLinkGatheredData: '',
-	finalUrl: '',
-	shouldRenderWebViewExclusively: false,
+	isOnline: false, //TODO ADD CHECK AND PUT IT HERE
+	firebaseUrlBasis: '',
+	parsedOneLinkAdCampaignName: '',
+	deepLinkParsedData: '',
+	theXValue: '',
+	openAtInstallUrl: '',
+	didOpenInstallUrl: false,
+	lastOpenedUrl: '',
+	renderWebView: false,
+	renderGame: false,
 };
 
 export const webViewSlice = createSlice({
 	name: 'webViewData',
 	initialState,
 	reducers: {
-		setRemoteConfigUrl: (
-			state,
-			{payload}: PayloadAction<string>) => {
-			state.remoteConfigUrl = payload;
-		},
-		setFinalUrl: (state, {payload}: PayloadAction<string>) => {
-			state.finalUrl = payload;
-		},
-		setShouldRenderWebViewExclusively: (
-			state,
-			{payload}: PayloadAction<boolean>) => {
-			state.shouldRenderWebViewExclusively = payload;
-		},
+		setIsOnline:
+			(state, {payload}: PayloadAction<boolean>) => {
+				state.isOnline = payload;
+			},
+		setFirebaseUrlBasis:
+			(state, {payload}: PayloadAction<string>) => {
+				state.firebaseUrlBasis = payload;
+			}, //ok | is it?
+		setParsedOneLinkAdCampaignName:
+			(state, {payload}: PayloadAction<string>) => {
+				state.parsedOneLinkAdCampaignName = payload;
+			}, //ok
+		setDeepLinkParsedData:
+			(state, {payload}: PayloadAction<string>) => {
+				state.deepLinkParsedData = payload;
+			}, //ok
+		setTheXValue:
+			(state, {payload}: PayloadAction<string>) => {
+				state.theXValue = payload;
+			}, //ok | is it?
+		setOpenAtInstallUrl:
+			(state, {payload}: PayloadAction<string>) => {
+				state.openAtInstallUrl = payload;
+			}, //ok
+		setDidOpenInstallUrl:
+			(state, {payload}: PayloadAction<boolean>) => {
+				state.didOpenInstallUrl = payload;
+			}, //ok
+		setLastOpenedUrl:
+			(state, {payload}: PayloadAction<string>) => {
+				state.lastOpenedUrl = payload;
+			}, //ok
+		setRenderWebView:
+			(state, {payload}: PayloadAction<boolean>) => {
+				state.renderWebView = payload;
+			}, //ok
+		setRenderGame:
+			(state, {payload}: PayloadAction<boolean>) => {
+				state.renderGame = payload;
+			}, //ok
 	},
 });
 
 export const {
-	setRemoteConfigUrl,
-	setFinalUrl,
-	setShouldRenderWebViewExclusively,
+	setIsOnline,
+	setFirebaseUrlBasis,
+	setParsedOneLinkAdCampaignName,
+	setDeepLinkParsedData,
+	setTheXValue,
+	setOpenAtInstallUrl,
+	setDidOpenInstallUrl,
+	setLastOpenedUrl,
+	setRenderWebView,
+	setRenderGame,
 } = webViewSlice.actions;
 
 export const selectWebViewData =
 	(state: { webViewSlice: WebViewSliceState; }) => state.webViewSlice;
-// export const selectWebViewData = (state: RootState) => state.webViewSlice;
 
 export const webViewSliceReducer = webViewSlice.reducer;
---
+++ redux/stateSlices/webViewSliceDEPRECATED.ts
@@ -0,0 +1,49 @@
+//@ts-nocheck
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+// import { RootState } from '../store';
+
+
+export interface WebViewSliceState {
+	remoteConfigUrl: string,
+	deepLinkGatheredData: string,
+	finalUrl: string,
+	shouldRenderWebViewExclusively: boolean,
+};
+const initialState: WebViewSliceState = {
+	remoteConfigUrl: '',
+	deepLinkGatheredData: '',
+	finalUrl: '',
+	shouldRenderWebViewExclusively: false,
+};
+
+export const webViewSlice = createSlice({
+	name: 'webViewData',
+	initialState,
+	reducers: {
+		setRemoteConfigUrl: (
+			state,
+			{payload}: PayloadAction<string>) => {
+			state.remoteConfigUrl = payload;
+		},
+		setFinalUrl: (state, {payload}: PayloadAction<string>) => {
+			state.finalUrl = payload;
+		},
+		setShouldRenderWebViewExclusively: (
+			state,
+			{payload}: PayloadAction<boolean>) => {
+			state.shouldRenderWebViewExclusively = payload;
+		},
+	},
+});
+
+export const {
+	setRemoteConfigUrl,
+	setFinalUrl,
+	setShouldRenderWebViewExclusively,
+} = webViewSlice.actions;
+
+export const selectWebViewData =
+	(state: { webViewSlice: WebViewSliceState; }) => state.webViewSlice;
+// export const selectWebViewData = (state: RootState) => state.webViewSlice;
+
+export const webViewSliceReducer = webViewSlice.reducer;
